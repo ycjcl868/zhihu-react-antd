2bbb7d89ba7a4ed64d46bbb42f2cf9409b8bb30e
信鑫-King <45808948@qq.com>
2017-06-14 16:53:08 +0800

    upd: loading fix: webpack css config bug

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import _ from 'lodash'
 import axios from 'axios'
-import { Row, Col, Button,message } from 'antd'
+import { Row, Col, Button, message, Spin } from 'antd'
 
 import '../style.less'
 
@@ -26,6 +26,31 @@ export default class Main extends Component {
                 const posts = res.data;
                 this.setState({
                     items: this.sortQuestion(posts)
+                })
+            })
+            .catch(err => {
+                const items = [
+                    {
+                        "id": 1,
+                        "title": "Mock坏了，这是本地加载的数据",
+                        "content": "理性探讨，请勿撕逼。产品经理的主要工作职责是产品设计。接受来自其他部门的需求，经过设计后交付研发。但这里有好些职责不清楚的地方。",
+                        "num": 120
+                    },
+                    {
+                        "id": 2,
+                        "title": "热爱编程是一种怎样的体验？",
+                        "content": "别人对玩游戏感兴趣，我对写代码、看技术文章感兴趣；把泡github、stackoverflow、v2ex、reddit、csdn当做是兴趣爱好；遇到重复的工作，总想着能不能通过程序实现自动化；喝酒的时候把写代码当下酒菜，边喝边想边敲；不给工资我也会来加班；做梦都在写代码",
+                        "num": 10
+                    },
+                    {
+                        "id": 3,
+                        "title": "热爱编程是一种怎样的体验？",
+                        "content": "别人对玩游戏感兴趣，我对写代码、看技术文章感兴趣；把泡github、stackoverflow、v2ex、reddit、csdn当做是兴趣爱好；遇到重复的工作，总想着能不能通过程序实现自动化；喝酒的时候把写代码当下酒菜，边喝边想边敲；不给工资我也会来加班；做梦都在写代码。",
+                        "num": 103
+                    }
+                ]
+                this.setState({
+                    items: this.sortQuestion(items)
                 })
             })
     }
@@ -67,6 +92,13 @@ export default class Main extends Component {
         })
     }
     render () {
+        if(!this.state.items.length) {
+            return (
+                <div className='loading'>
+                    <Spin size="large" />
+                </div>
+            )
+        }
         return (
             <div>
                 <Row className='header' justify='center'>
